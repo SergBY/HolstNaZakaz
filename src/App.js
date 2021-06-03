e1e62880a45f396aec44900bc29da00ea2cc46ab
SergBY <57902822+SergBY@users.noreply.github.com>
2021-06-03 13:23:49 +0300

    add: component CollagePage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/pages/homePage/HomePage';
 import PrintingPage from './components/pages/printingPage/PrintingPage';
+import CollagePage from './components/pages/collagePage/CollagePage';
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
       {/* <PrintingPage/> */}
+      {/* <CollagePage/> */}
     </div>
   );
 }
---
+++ src/components/pages/collagePage/CollagePage.js
@@ -0,0 +1,18 @@
+import Header from '../../header/Header';
+import SectionStart from '../../sectionStart/SectionStart';
+import SectionCollage from './sectionCollage/SectionCollage';
+
+import Footer from '../../footer/Footer';
+
+function PrintingPage() {
+  return (
+    <>
+      <Header/>
+      <SectionStart/>
+      <SectionCollage/>
+      <Footer/>
+    </>
+  );
+}
+
+export default PrintingPage;
