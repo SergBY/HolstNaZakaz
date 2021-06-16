1d425d4131a669cf3b98bcd099ec69d3026b801c
SergBY <57902822+SergBY@users.noreply.github.com>
2021-06-17 01:00:20 +0300

    add: modal window for login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css';
+import Modal from '../contacts/Pop-up/Contacts_Pop-up';
 
 function Login() {
- return (
-  <div className="menu__login">
-    <button className="menu__login-btn">Войти</button>
-    <button className="menu__registration-btn" href="#">Регистрация</button>
-  </div>
- ) 
+  const [modalActive, setModalActive] = useState(false);
+  return (
+    <>
+      <div className="menu__login">
+        <button className="menu__login-btn" onClick={() => setModalActive(true)}>Войти</button>
+        <button className="menu__registration-btn" href="#">Регистрация</button>
+      </div>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <p>dfsdf</p>
+      </Modal>
+    </>
+  ) 
 }
 
 export default Login;
